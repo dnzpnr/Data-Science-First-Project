692951e80709e2bbd0984a6a6d2d481f14e8d735
dnzpnr <dnz1pnr@gmail.com>
2021-06-11 22:49:00 +0300

    My first project

---
+++ EDA_II.py
@@ -0,0 +1,43 @@
+#############################################
+# EXPLORATORY DATA ANALYSIS
+#############################################
+
+# 1. users ve purchases veri setlerinin okutulması ve veri setlerinin "uid" değişkenine göre inner join ile merge edilmesi.
+
+# 2. Kendi yazacağın fonksiyon ile veri setinin genel özelliklerinin betimlenmesi.
+# İpucu: Observations, Variables, categorical variables, numerical variables, numeric but categorical
+# (Sayısal değişken ama 20 sınıf ya da daha az sayıda sınıfı olan değişken sayısı),
+# categaorical but cardinal(Kategorik fakat sınıf sayısı çok fazla olan değişken sayısı (20))
+
+# Beklenen çıktı:
+
+# ----> helpers_function(df)
+
+# Observations: 9006
+# Variables: 8
+# cat_cols: 4
+# num_cols: 2
+# cat_but_car: 2
+# num_but_cat: 1
+
+# 3. Veri setinin Shape, Types, Head, Tail, NA, Quantiles verilerini bize getiren fonksiyonu tanımlama.
+
+# 4. Kaç unique müşteri vardır?
+
+# 5. Kaç unique fiyatlandırma var?
+
+# 6. Hangi fiyattan kaçar tane satılmış?
+
+# 7. Hangi ülkeden kaçar tane satış olmuş?
+
+# 8. Ülkelere göre satışlardan toplam ne kadar kazanılmış?
+
+# 9. Device türlerine göre satış sayıları nedir?
+
+# 10. ülkelere göre fiyat ortalamaları nedir?
+
+# 11. Cihazlara göre fiyat ortalamaları nedir?
+
+# 12. Ülke-Cihaz kırılımında fiyat ortalamaları nedir?
+
+# 13. country, device, gender, age kırılımında toplam kazançlar nedir?
